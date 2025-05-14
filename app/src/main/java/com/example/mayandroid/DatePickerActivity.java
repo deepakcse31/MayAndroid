2cd18a65b40de86b35374719c7afac649e17dd06
Deepak <deepakcse31@gmail.com>
2025-05-14 21:40:57 +0530

    DatePicker

---
+++ app/src/main/java/com/example/mayandroid/DatePickerActivity.java
@@ -0,0 +1,54 @@
+package com.example.mayandroid;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class DatePickerActivity extends AppCompatActivity {
+    TextView tvselectdate;
+    Button btnselectdate;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_date_picker);
+        btnselectdate=findViewById(R.id.btnselectdate);
+        tvselectdate=findViewById(R.id.tvselectdate);
+
+        btnselectdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
+                int year=calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog=new DatePickerDialog(DatePickerActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        String date=String.valueOf(i2)+"/"+String.valueOf(i1+1)+"/"+String.valueOf(i);
+                        tvselectdate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
